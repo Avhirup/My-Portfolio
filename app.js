9e5129b210fbd8476fe831dddcafa1d64ea70c69
Avhi <avhirup0220@gmail.com>
2023-01-19 14:31:27 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const sections = document.querySelectorAll(".section");
+const sectBtns = document.querySelectorAll(".controlls");
+const sectBtn = document.querySelectorAll(".control");
+
+const allSections = document.querySelector(".main-content");
+
+
+// To switch active-btn class between differnt buttons
+
+function PageTransitions() {
+    // Button click active class
+    for (let i = 0; i < sectBtn.length; i++) {
+        sectBtn[i].addEventListener('click', function (event) {
+            let currentBtn = document.querySelectorAll('.active-btn');
+            // currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            // this.className += ' active-btn';
+            currentBtn[0].classList.remove('active-btn');
+            event.target.classList.add('active-btn');
+        })
+    }
+
+    // Sections Active class
+    allSections.addEventListener("click", (event) => {
+        const id = event.target.dataset.id;
+        if (id) {
+            // Remove selected from the other btns
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active')
+            })
+            event.target.classList.add('active');
+
+            // hide other sections
+            sections.forEach((section) => {
+                section.classList.remove('active');
+            })
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
+
+
+}
+
+PageTransitions();
