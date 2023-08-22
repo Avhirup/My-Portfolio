829e603700350125e62b7769a783b3379ef3a7d3
Avhi <avhirup0220@gmail.com>
2023-08-23 00:01:24 +0530

    Added Responsiveness and Animated Icons

--- app.js
+++ app.js
@@ -15,7 +15,11 @@ function PageTransitions() {
             // currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
             // this.className += ' active-btn';
             currentBtn[0].classList.remove('active-btn');
-            event.target.classList.add('active-btn');
+            let id = event.target.dataset.id + 'Btn';
+            console.log(id);
+            const btnToActive = document.getElementById(id);
+            console.log(btnToActive)
+            btnToActive.classList.add('active-btn');
         })
     }
 
@@ -24,10 +28,10 @@ function PageTransitions() {
         const id = event.target.dataset.id;
         if (id) {
             // Remove selected from the other btns
-            sectBtns.forEach((btn) => {
-                btn.classList.remove('active')
-            })
-            event.target.classList.add('active');
+            // sectBtns.forEach((btn) => {
+            //     btn.classList.remove('active-btn');
+            // })
+            // event.target.classList.add('active-btn');
 
             // hide other sections
             sections.forEach((section) => {
@@ -39,13 +43,41 @@ function PageTransitions() {
     })
 
     // Toggle theme
-    const themeBtn = document.querySelector('.theme-btn');
-    themeBtn.addEventListener('click', () => {
-        let element = document.body;
-        element.classList.toggle('light-mode');
-    })
+    // const themeBtn = document.querySelector('.theme-btn');
+    // themeBtn.addEventListener('click', () => {
+    //     let element = document.body;
+    //     element.classList.toggle('light-mode');
+    //     let backgroundColor = getComputedStyle(document.body).getPropertyValue('--color-primary').trim();
+    //     console.log(backgroundColor)
+    //     //! CHANGING ADDRESS BAR AND STATUS BAR'S COLOR FOR MOBILE DEVICES
+    //     document.querySelector('meta[name="theme-color"]').setAttribute('content', backgroundColor);
+    // document.querySelector('meta[name="msapplication-navbutton-color"]').setAttribute('content', backgroundColor);
+    // document.querySelector('meta[name="apple-mobile-web-app-status-bar-style"]').setAttribute('content', backgroundColor);
+    //     console.log('clicked');
+    // })
 
+    const checkbox = document.getElementById("checkbox")
+    checkbox.addEventListener("change", () => {
+        document.body.classList.toggle('light-mode')
+
+        //! CHANGING ADDRESS BAR AND STATUS BAR'S COLOR FOR MOBILE DEVICES
+        let backgroundColor = getComputedStyle(document.body).getPropertyValue('--color-primary').trim();
+        document.querySelector('meta[name="theme-color"]').setAttribute('content', backgroundColor);
+        document.querySelector('meta[name="msapplication-navbutton-color"]').setAttribute('content', backgroundColor);
+        document.querySelector('meta[name="apple-mobile-web-app-status-bar-style"]').setAttribute('content', backgroundColor);
+        console.log('clicked');
+    })
 
 }
 
 PageTransitions();
+
+// trying
+
+function del() {
+    document.getElementById("textarea1").value = '';
+}
+function clearText() {
+    setTimeout(del, 2000);
+}
+
